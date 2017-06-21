2e006bb4a72633e7401d79f677106e2ebbb1b2b3
Justin Livi <justin@brinkbit.com>
2017-06-21 15:47:24 -0400

    Refactor into class and add client_token request

--- src/index.js
+++ src/index.js
@@ -1,36 +1,90 @@
 const request = require( 'request' );
 const bodyParser = require( 'body-parser' );
 const pathToRegexp = require( 'path-to-regexp' );
+const merge = require( 'lodash.merge' );
+const validate = require( 'validate.js' );
 
-module.exports = function createSDK( options ) {
-    return [
-        bodyParser.json(),
-        function setPath( req, res, next ) {
-            const headers = {};
-            const body = req.body;
-            if ( req.headers.authorization ) {
-                headers.Authorization = req.headers.authorization;
-            }
-            if ( pathToRegexp( '/token/' ).test( req.path ) && body.grant_type === 'password' ) {
-                body.client_id = body.client_id || options.gameId;
-                body.client_secret = options.secretKey;
-            }
-            request({
-                method: req.method,
-                url: `${options.base || 'https://brinkbit.com'}/api/${options.apiVersion || '0.1'}${req.path}`,
-                headers,
-                form: body,
-                json: true,
-            }, ( error, response, body ) => {
-                if ( error ) {
-                    next( error );
+validate.validators.dataType = function validateDataType( value, options ) {
+    return ( value === null || value === undefined || validate[`is${validate.capitalize( options )}`]( value )) ? null : `is not of type ${options}`;
+};
+
+module.exports = class Brinkbit {
+
+    constructor( options ) {
+        validate( options, {
+            gameId: {
+                dataType: 'string',
+                presence: true,
+            },
+            secretKey: {
+                dataType: 'string',
+                presence: true,
+            },
+            base: {
+                dataType: 'string',
+            },
+            scope: {
+                dataType: 'string',
+            },
+        });
+        this.options = options;
+    }
+
+    createMiddleware( options ) {
+        const opts = merge({}, this.options, options );
+        return [
+            bodyParser.json(),
+            ( req, res, next ) => {
+                if ( this.access_token ) {
+                    return next();
+                }
+                request({
+                    method: 'post',
+                    url: `${opts.base || 'https://brinkbit.com'}/api/${opts.apiVersion || '0.1'}/token/`,
+                    form: {
+                        client_id: req.body.client_id || opts.gameId,
+                        client_secret: opts.secretKey,
+                        grant_type: 'client_credentials',
+                        scope: opts.scope || 'player:create data:read:write',
+                    },
+                    json: true,
+                }, ( error, response, body ) => {
+                    if ( body && body.access_token ) {
+                        this.access_token = body.access_token;
+                    }
+                    next();
+                });
+            },
+            ( req, res, next ) => {
+                const headers = {};
+                const body = req.body;
+                if ( req.headers.authorization ) {
+                    headers.Authorization = req.headers.authorization;
                 }
                 else {
-                    res.set( response.headers );
-                    res.status( response.statusCode );
-                    res.send( body );
+                    headers.Authorization = `Bearer ${this.access_token}`;
                 }
-            });
-        },
-    ];
-};
+                if ( pathToRegexp( '/token/' ).test( req.path ) && body.grant_type === 'password' ) {
+                    body.client_id = body.client_id || opts.gameId;
+                    body.client_secret = opts.secretKey;
+                }
+                request({
+                    method: req.method,
+                    url: `${opts.base || 'https://brinkbit.com'}/api/${opts.apiVersion || '0.1'}${req.path}`,
+                    headers,
+                    form: body,
+                    json: true,
+                }, ( error, response, body ) => {
+                    if ( error ) {
+                        next( error );
+                    }
+                    else {
+                        res.set( response.headers );
+                        res.status( response.statusCode );
+                        res.send( body );
+                    }
+                });
+            },
+        ];
+    }
+}
